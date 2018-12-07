02ed39f72d8da3c8aabb383c873ef2d7f9c6c62c
xiaopingfeng <fxp007@gmail.com>
2018-12-07 17:25:13 +0800

    log position into database

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,10 +1,20 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+var PouchDB = require('pouchdb');
+var db = new PouchDB('http://116.85.30.223:5984/traccar');
+db.info().then(function (info) {
+    console.log(info);
+})
+
+var flatten = require('flat')
 
 router.post('/', function (req, res, next) {
-    console.log(req.body);
+    console.log(req.body.device.uniqueId, req.body.position);
+    let log = flatten(req.body);
+    log.timestamp = new Date(req.body.position.fixTime).getTime()
+    db.put(flatten(req.body));
+
     res.send('ok');
 })
 
@@ -15,3 +25,5 @@ router.get('/', function (req, res, next) {
 });
 
 module.exports = router;
+
+
